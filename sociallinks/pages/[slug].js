b1cff6ec7a181577ed809a0e1a09c4ea4c6b2e16
netomatiazi <netoomn@gmail.com>
2020-10-07 22:27:27 -0300

    Creating application and urging dependencies of Next, Prismic, React and React Dom

---
+++ sociallinks/pages/[slug].js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const redirectTo = () => {
+    return <h1>Redirect</h1>
+}
+
+export default redirectTo; 
---
+++ sociallinks/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// Criando componente index
+
+const Index = () => {
+    return <h1>Social Links (central de links)</h1>
+}
+export default Index;
