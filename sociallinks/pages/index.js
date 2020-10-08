f9dd49d6907598619074dcdd366e63bd29396191
netomatiazi <netoomn@gmail.com>
2020-10-08 10:37:30 -0300

    Linking the backend in the cloud created using prismíc with the application, to view the links created

--- sociallinks/pages/index.js
+++ sociallinks/pages/index.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import Prismic from 'prismic-javascript'
 
 // Criando componente index
 
-const Index = () => {
-    return <h1>Social Links (central de links)</h1>
+const Index = ({ data }) => {
+    console.log('Client')
+    return (
+        <div style={{ backgroundColor: data.background, color: data.text }}>
+            <h1>Social Links (central de links)</h1>
+            <ul>
+                {/* Função que transforma um vetor em uma lista de elementos renderizáveis na tela  */}
+                {
+                    data.body.map(item => {
+                        return (<li><a href={item.primary.destino.url}>{item.primary.texto_do_botao}</a></li>)
+                    })
+                }
+            </ul>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    )
+
 }
+
+// Pegando os links que estão dentro da central de links
+export async function getServerSideProps() {
+    // Função faz com que a página sempre seja renderizada no servidor
+    console.log('server')
+    //Criando um novo cliente para o prismic
+    const client = Prismic.client('https://omnweb.cdn.prismic.io/api/v2')
+    const centralLinks = await client.getSingle('centrallinks')
+    console.log(centralLinks)
+    return {
+        props: {
+            data: centralLinks.data, //
+        },
+
+    }
+}
+
 export default Index;
