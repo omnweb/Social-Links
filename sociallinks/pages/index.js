1dee665c5186588e696a3372ecc8c1d70f323f3b
netomatiazi <netoomn@gmail.com>
2020-10-08 11:34:57 -0300

    Including solving the challenge of the second lesson to put an image in the prismic and present it on the home screen.

--- sociallinks/pages/index.js
+++ sociallinks/pages/index.js
@@ -2,21 +2,29 @@ import React from 'react'
 import Prismic from 'prismic-javascript'
 
 // Criando componente index
-
 const Index = ({ data }) => {
     console.log('Client')
     return (
+
         <div style={{ backgroundColor: data.background, color: data.text }}>
             <h1>Social Links (central de links)</h1>
             <ul>
                 {/* Função que transforma um vetor em uma lista de elementos renderizáveis na tela  */}
                 {
                     data.body.map(item => {
-                        return (<li><a href={item.primary.destino.url}>{item.primary.texto_do_botao}</a></li>)
+                        if (item.slice_type === 'image') {
+                            return <img src={item.primary.perfil.url} alt="Imagem de Perfil"></img>
+                        }
+                        else if (item.slice_type === 'secao') {
+                            return <h2>{item.primary.nome}</h2>
+                        } else if (item.slice_type === 'link') {
+                            return (<li><a href={item.primary.destino.url}>{item.primary.texto_do_botao}</a></li>)
+                        }
+                        else { return null }
                     })
                 }
             </ul>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
         </div>
     )
 
