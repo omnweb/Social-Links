a2af01ad62627cb7f8695d2a94616980b531552e
netomatiazi <netoomn@gmail.com>
2020-10-10 12:00:12 -0300

    Creating the link that leads to linkedin in Prismic by capturing in the application and implementing invalid url redirects.

--- sociallinks/pages/[slug].js
+++ sociallinks/pages/[slug].js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react' // Usando para fazer hook
+import Prismic from 'prismic-javascript'
+import { useRouter } from 'next/router' // Importando user router do next
+
 
 const redirectTo = () => {
-    return <h1>Redirect</h1>
+    const router = useRouter()
+    useEffect(() => {
+        console.log('Carregou...')
+        setTimeout(() => {
+            console.log('Redireciona...')
+            router.push("/") // Redirecionando para o raiz usando userRouter do next
+        }, 3000)
+    }, [])
+    return (
+        <div className="redirect">
+            <h1>Página não foi encontrada</h1>
+            <p>Rentonando para a página de link em segudos...</p>
+        </div>
+    )
+}
+
+export async function getServerSideProps({ params, res }) {
+    const client = Prismic.client('https://omnweb.cdn.prismic.io/api/v2')
+    const link = await client.getByUID('shortlink', params.slug)
+    if (link) {
+        res.statusCode = 301 // Conteúdo removido permanentemente
+        res.setHeader('Location', link.data.destino.url) // Redireciona
+        res.end()
+        return
+    }
+    return {
+        props: {},
+    }
 }
 
 export default redirectTo; 
